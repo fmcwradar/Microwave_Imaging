847f8074c626a197b44e0ef5702fde6f7089dcbc
fmcwradar <m.maier@tu-braunschweig.de>
2024-08-21 09:07:37 +0200

    Update Prepare_Radar_Data.py

--- Prepare_Radar_Data.py
+++ Prepare_Radar_Data.py
@@ -1,27 +1,31 @@
-#Import of Necessary Python Libraries
+#Import of Necessary Python Libraries.
 import scipy.constants as sc
 import matplotlib.pyplot as plt
 import numpy as np
-#Import of the Radar Measurement Evaluation Modul
+import os
+#Import of the Radar Measurement Evaluation Modul.
 from Radar_Evaluation_Modul import radar_measurement_evaluation
-
-#Target
-path = r"G:\Meine Ablage\Finaler Vergleich Radar vs. VNA\02-07-2024\Bassi\Radar"
+#Get current directory.
+current_dir = os.path.dirname(__file__)
+#Path in which the .csv-files are located.
+path = r"{0}\Ideal Data Radar".format(current_dir)
 number_of_files = 45
 
 file_names = []
 
 for number in range(0, number_of_files+1):
     file_names.append("{0}".format(number))
-    
+
+#Define settings for Radar_Evaluation_Modul.
 f0 = 6e9
 f1 = 14e9
 B = (f1-f0)
 T_c = 200*1e-6
 c0 = sc.speed_of_light
-number_of_ramps = 300
+number_of_ramps = 1
 total = 4096
 windowing = True
+ideal = True
 
 measurement_list_no_target = []
 measurement_list_time_domain_up = []
@@ -32,7 +36,7 @@ spectrum_matrix = np.zeros((len(file_names),total), dtype = complex)
 #Iterate over the measurements.
 for count, name in enumerate(file_names):
     print(count)
-    single_measurement = radar_measurement_evaluation(path,name,B,T_c,c0,number_of_ramps,total,f0,f1,windowing)
+    single_measurement = radar_measurement_evaluation(path,name,B,T_c,c0,number_of_ramps,total,f0,f1,windowing,ideal)
     single_measurement.run_radar_evaluation()
     spectrum_matrix[count,:] = single_measurement.spectrum_up
     
