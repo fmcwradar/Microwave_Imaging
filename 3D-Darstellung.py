The multithreaded image calculation is now memory efficient. Swap_IQ = True needed for correct calculation of image.

3D-Darstellung.py
@@ -25,18 +25,20 @@ elif pc_name == "MJ-XMG":
 
 current_dir = os.path.dirname(__file__)
 working_dir = r"D:\Microwave_Imaging\Pickle_Files\2025-03-20_11-37-56"
+save_animation = False
 
 with open(f"{working_dir}\\image_radar.pkl", 'rb') as file:
 # with open(f"{working_dir}\Final_3D_Files\\final_3D_image.pkl", 'rb') as file:
     # Call load method to deserialze.
     vol2 = pickle.load(file)
 
-    # intensity_matrix = vol2 ** 2
-    # intensity_matrix_norm = np.abs(intensity_matrix) / np.max(np.abs(intensity_matrix))
-    print(f"{np.max(np.abs(vol2))}")
-    intensity_matrix_db = 20 * np.log10(vol2)
+    intensity_matrix = vol2 ** 2
+    intensity_matrix_norm = np.abs(intensity_matrix) / np.max(np.abs(intensity_matrix))
+    # print(f"{np.max(np.abs(vol2))}")
+    intensity_matrix_db = 20 * np.log10(intensity_matrix_norm)
+    print(intensity_matrix_db.dtype)
 
-    image_matrix_3D = vol2[:, :, :].astype(np.float32)
+    image_matrix_3D = np.abs(vol2[:, :, :]).astype(np.float32)
     image_matrix_3D_db = intensity_matrix_db[:, :, :].astype(np.float32)
 
     vol2 = image_matrix_3D_db
@@ -44,26 +46,23 @@ with open(f"{working_dir}\\image_radar.pkl", 'rb') as file:
     rearranged_vol2 = np.swapaxes(vol2, 0, 2)
     rearranged_vol2 = np.flip(rearranged_vol2, 2)
     rearranged_vol2 = np.swapaxes(rearranged_vol2, 2, 1)
-    rearranged_vol2 = np.flip(rearranged_vol2, 1)
+    # rearranged_vol2 = np.flip(rearranged_vol2, 1)
     vol = rearranged_vol2
+    # vol = vol2
 
 with open(f"{working_dir}\\Settings.txt", "r") as file:
     lines = file.readlines()
 
-    offset = int(lines[14].split("= ")[1])
+    offset = float(lines[14].split("= ")[1])
     start_x = int(lines[15].split(" cm\n")[0].split("start_x = ")[1])
     start_y = int(lines[16].split(" cm\n")[0].split("start_y = ")[1])
     start_z = int(lines[17].split(" cm\n")[0].split("start_z = ")[1])
     end_x = int(lines[18].split(" cm\n")[0].split("end_x = ")[1])
     end_y = int(lines[19].split(" cm\n")[0].split("end_y = ")[1])
     end_z = int(lines[20].split(" cm\n")[0].split("end_z = ")[1])
-    number_of_points_x = int(lines[24].split("\n")[0].split("Number of points x = ")[1])
-    number_of_points_y = int(lines[25].split("\n")[0].split("Number of points y = ")[1])
-    number_of_points_z = int(lines[26].split("\n")[0].split("Number of points z = ")[1])
-
-print("start_x:", start_x)
-print("end_x:", end_x)
-print("number_of_points_x:", number_of_points_x)
+    number_of_points_x = int(lines[22].split("\n")[0].split("Number of points x = ")[1])
+    number_of_points_y = int(lines[23].split("\n")[0].split("Number of points y = ")[1])
+    number_of_points_z = int(lines[24].split("\n")[0].split("Number of points z = ")[1])
 
 x_axis = np.linspace(start_x, end_x, number_of_points_x)
 y_axis = np.linspace(start_y, end_y, number_of_points_y)
@@ -73,7 +72,7 @@ z_axis = np.linspace(start_z, end_z, number_of_points_z)
 canvas = scene.SceneCanvas(keys='interactive', size=(1000, 1000), show=True)
 view1 = canvas.central_widget.add_view()
 
-clim = (-40, 0)
+clim = (-30, 0)
 texture_format = 'auto'
 
 volume = scene.visuals.Volume(
@@ -290,33 +289,34 @@ canvas.events.resize.connect(on_canvas_resize)
 
 place_slice_plots("3D")
 
-import numpy as np
-import imageio
-from vispy import scene, app
+if save_animation == True:
+    import numpy as np
+    import imageio
+    from vispy import scene, app
 
-# Save video frames
-frames = []
-num_frames = 180  # Number of frames in the loop
+    # Save video frames
+    frames = []
+    num_frames = 180  # Number of frames in the loop
 
-for i in range(int(num_frames/2)):
-    view1.camera.azimuth += 180 / num_frames  # Rotate camera
-    canvas.update()  # Update canvas
-    frame = canvas.render()  # Capture frame
-    frames.append(frame)
+    for i in range(int(num_frames/2)):
+        view1.camera.azimuth += 180 / num_frames  # Rotate camera
+        canvas.update()  # Update canvas
+        frame = canvas.render()  # Capture frame
+        frames.append(frame)
 
-for i in range(int(num_frames/2)):
-    view1.camera.azimuth -= 180 / num_frames  # Rotate camera
-    canvas.update()  # Update canvas
-    frame = canvas.render()  # Capture frame
-    frames.append(frame)
+    for i in range(int(num_frames/2)):
+        view1.camera.azimuth -= 180 / num_frames  # Rotate camera
+        canvas.update()  # Update canvas
+        frame = canvas.render()  # Capture frame
+        frames.append(frame)
 
-# ✅ Save as MP4 using FFmpeg
-imageio.mimsave(f"Test.mp4", frames, format="FFMPEG", fps=30)
+    # ✅ Save as MP4 using FFmpeg
+    imageio.mimsave(f"Test.mp4", frames, format="FFMPEG", fps=30)
 
-# ✅ Save as GIF (Optional)
-imageio.mimsave(f"Test.gif", frames, duration=1/30)  # 30 FPS
+    # ✅ Save as GIF (Optional)
+    imageio.mimsave(f"Test.gif", frames, duration=1/30)  # 30 FPS
 
-print("MP4 and GIF saved successfully!")
+    print("MP4 and GIF saved successfully!")
 
 if __name__ == '__main__':
     canvas.show()

Calculate_Radar_Image_Multithreaded.py
@@ -79,18 +79,18 @@ T_c = 200*1e-6
 c0 = sc.speed_of_light
 number_of_ramps = 50
 total = 8192
-windowing = False
-ideal = True
-swap_IQ = False
-calibration = False
-filtering = False
+windowing = True
+ideal = False
+swap_IQ = True
+calibration = True
+filtering = True
 fc_low = 350e3
 fc_high = 500e3
 filterorder = 10
 save_last_measurement = False
 background_subtraction = False
 hilbert = False
-offset = 0
+offset = 0.935
 #End of define settings.
 
 # list_of_measurements = []
@@ -103,51 +103,73 @@ offset = 0
 
 def main():
     global radar_imaging
-    # Pool(8) means 8 threads are used to process multiple files at the same time
-    with Pool(8) as pool:
-        # tqdm generates Progressbar with length=total
-        with tqdm(total=len(file_info), desc="Preparing files", unit=" file", file=sys.stdout) as pbar:
-            # Use imap_unordered to process files and update progress bar
-            for i, single_measurement in enumerate(pool.imap_unordered(process_data_in_file, file_info)):
-                list_of_measurements.append(single_measurement)
-                pbar.update(1)
 
     # Save the result in .pkl-file and create log-file.
     current_dir = os.path.dirname(__file__)
     from datetime import datetime
     date_today = datetime.today().strftime('%Y-%m-%d')
     time_today = datetime.today().strftime('%H-%M-%S')
-
     path = pathlib.Path(r'{0}\Pickle_Files\{1}_{2}'.format(current_dir, date_today, time_today))
     path.mkdir(parents=True, exist_ok=True)
+    prepared_data_path = pathlib.Path(f"{path}\Prepared_Data")
+    prepared_data_path.mkdir(parents=True, exist_ok=True)
 
-    with open(r"{0}\prepared_data.pkl".format(path), 'wb') as file:
-        # A new file will be created.
-        pickle.dump(list_of_measurements, file)
+    # Pool(8) means 8 threads are used to process multiple files at the same time
+    with Pool(8) as pool:
+        # tqdm generates Progressbar with length=total
+        with tqdm(total=len(file_info), desc="Preparing files", unit=" file", file=sys.stdout) as pbar:
+            # Use imap_unordered to process files and update progress bar
+            for single_measurement in pool.imap_unordered(process_data_in_file, file_info):
+                # list_of_measurements.append(single_measurement)
+                with open(f"{prepared_data_path}\{single_measurement.name}.pkl", 'wb') as file:
+                    # A new file will be created.
+                    pickle.dump(single_measurement.spectrum_up, file)
+                pbar.update(1)
+
+    # Save the result in .pkl-file and create log-file.
+    # current_dir = os.path.dirname(__file__)
+    # from datetime import datetime
+    # date_today = datetime.today().strftime('%Y-%m-%d')
+    # time_today = datetime.today().strftime('%H-%M-%S')
+    #
+    # path = pathlib.Path(r'{0}\Pickle_Files\{1}_{2}'.format(current_dir, date_today, time_today))
+    # path.mkdir(parents=True, exist_ok=True)
+    # prepared_data_path = pathlib.Path(f"{path}\Prepared_Data")
+    # prepared_data_path.mkdir(parents=True, exist_ok=True)
+    #
+    # for i in range(0, len(list_of_measurements)):
+    #     # Save the individual measurement objects for more efficient memory usage during image calculation
+    #     with open(f"{prepared_data_path}\{list_of_measurements[i].name}.pkl", 'wb') as file:
+    #         # A new file will be created.
+    #         pickle.dump(list_of_measurements[i], file)
 
     #Define settings for Radar_Imaging_Modul.
     distance = single_measurement.distance_corrected
     number_of_points_x = 201
     number_of_points_y = 201
-    number_of_points_z = 24
+    number_of_points_z = 201
     start_x = 0
     end_x = 45
     start_y = 40
-    end_y = 90
+    end_y = 80
     start_z = 0
     end_z = 23
 
     antenna_distance = 10
-    antenna_start = 0
-    antenna_end = 45
-    number_of_measurements = 46
-    dynamic_range = 40
+    # antenna_start = 0
+    # antenna_end = 45
+    # number_of_measurements = 46
+    dynamic_range = 30
     # image_matrix = np.zeros((number_of_points_z, number_of_points_y, number_of_points_x))
     #End of define settings.
 
-    settings = [f0,f1,B,np.round(T_c,8),number_of_ramps,total,windowing,calibration,filtering,fc_low,fc_high,filterorder,background_subtraction,hilbert,offset,start_x,start_y,start_z,end_x,end_y,end_z,antenna_distance,antenna_start,antenna_end,number_of_points_x,number_of_points_y,number_of_points_z,path_csv]
+    settings = [f0,f1,B,np.round(T_c,8),number_of_ramps,total,windowing,calibration,filtering,fc_low,fc_high,filterorder,background_subtraction,hilbert,offset,start_x,start_y,start_z,end_x,end_y,end_z,antenna_distance,number_of_points_x,number_of_points_y,number_of_points_z,path_csv, path]
 
-    radar_imaging = radar_imaging(list_of_measurements,distance,offset,number_of_points_x,number_of_points_y,number_of_points_z,start_x,start_y,start_z,end_x,end_y,end_z,antenna_distance,antenna_start,antenna_end,dynamic_range,number_of_measurements,settings)
+    # radar_imaging = radar_imaging(list_of_measurements,distance,offset,number_of_points_x,number_of_points_y,number_of_points_z,start_x,start_y,start_z,end_x,end_y,end_z,antenna_distance,antenna_start,antenna_end,dynamic_range,number_of_measurements,settings)
+    radar_imaging = radar_imaging(distance, offset, number_of_points_x, number_of_points_y,
+                                  number_of_points_z, start_x, start_y, start_z, end_x, end_y, end_z, antenna_distance,
+                                  dynamic_range, settings)
+
     radar_imaging.run_radar_imaging()
 
 if __name__ == "__main__":

Radar_Imaging_Modul_V02_Multithreaded.py
@@ -115,45 +115,6 @@ class radar_imaging:
 
         return self.image_matrix
 
-        # counter = counter + 1
-
-    def collect_and_calculate(self, calculation_info):
-        """
-        Function collect all data needed for the calculation of the image and calculates the image
-        """
-        global working_dir
-        path_pickle, distance, name, antenna_spacing = calculation_info
-        # with open(r'{0}\{1}'.format(path_pickle, name), 'rb') as file:
-        #     Call load method to deserialze.
-        #     spectrum = pickle.load(file)
-
-        # name = name[:-4]  # Delete the ".pkl" of the file name.
-
-        if not name.startswith("distance"):
-            image_matrix_cache = self.calculate_image()
-            print(type(image_matrix_cache))
-
-        return image_matrix_cache
-
-    # ax = sns.heatmap(10*np.log10(np.abs(image_matrix_norm)), cbar = True, cmap = 'jet',square = True, vmax = 0, vmin = -self.dynamic_range, cbar_kws={'label': 'Normalized Intensity (dB)'})
-    #
-    # x_ticks = (np.round(np.linspace(self.start_x, self.end_x, int(np.round((np.abs(self.end_x)+np.abs(self.start_x))/5,0)+1)),1))
-    # x_ticks_location = np.linspace(0, len(x_axis), int(np.round((np.abs(self.end_x)+np.abs(self.start_x))/5,0)+1))
-    #
-    # y_ticks = (np.round(np.linspace(self.start_y, self.end_y, int(np.round((np.abs(self.end_y)-np.abs(self.start_y))/5,0)+1)),1))
-    # y_ticks_location = np.linspace(0, len(y_axis), int(np.round((np.abs(self.end_y)-np.abs(self.start_y))/5,0)+1))
-    #
-    # ax.invert_yaxis()
-    # cbar_axes = ax.figure.axes[-1]
-    # ax.figure.axes[-1].yaxis.label.set_size(22.5)
-    # plt.xticks(x_ticks_location, x_ticks, fontsize = 22.5)
-    # plt.yticks(y_ticks_location, y_ticks, fontsize = 22.5)
-    # plt.xlabel("x (cm)", fontsize = 22.5)
-    # plt.ylabel("y (cm)", fontsize = 22.5)
-    # cax = ax.figure.axes[-1]
-    # cax.tick_params(labelsize=22.5)
-    # plt.show()
-
     def run_radar_imaging(self):
         plotting = True
         image_matrix = np.zeros((self.number_of_points_x, self.number_of_points_y, self.number_of_points_z), dtype=complex)
@@ -191,7 +152,7 @@ class radar_imaging:
 
         if plotting == True:
             plt.figure("Multithreaded")
-            ax = sns.heatmap(10 * np.log10(np.abs(image_matrix_norm)[:,:,0]), cbar=True, cmap='jet', square=True, vmax=0,
+            ax = sns.heatmap(10 * np.log10(np.abs(image_matrix_norm)[:,:,0].T), cbar=True, cmap='jet', square=True, vmax=0,
                              vmin=-self.dynamic_range, cbar_kws={'label': 'Normalized Intensity (dB)'})
 
             x_axis = np.linspace(self.start_x, self.end_x, self.number_of_points_x)